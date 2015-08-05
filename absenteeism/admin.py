24b40bd4caa8056884bd43e1227942aa5070a26c
Mario Gutierrez <mfdogutierrez@gmail.com>
2015-08-05 22:31:05 +0000

    Added models to the admin

--- absenteeism/admin.py
+++ absenteeism/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Diagnosis, Disease, Area, Organization, Employee, Record
+
+admin.site.register(Diagnosis)
+admin.site.register(Disease)
+admin.site.register(Area)
+admin.site.register(Organization)
+admin.site.register(Employee)
+admin.site.register(Record)
---
+++ absenteeism/migrations/0001_initial.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Area',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Diagnosis',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=200)),
+                ('description', models.TextField(max_length=600)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Disease',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('description', models.TextField(max_length=600)),
+                ('diagnosis', models.ForeignKey(to='absenteeism.Diagnosis')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Employee',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('personal_number', models.CharField(max_length=12)),
+                ('name', models.CharField(max_length=20)),
+                ('last_name', models.CharField(max_length=25)),
+                ('age', models.CharField(max_length=2)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Organization',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('area', models.ForeignKey(to='absenteeism.Area')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Record',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date', models.DateTimeField()),
+                ('disease', models.ForeignKey(to='absenteeism.Disease')),
+                ('employee', models.ForeignKey(to='absenteeism.Employee')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='employee',
+            name='organization',
+            field=models.ForeignKey(to='absenteeism.Organization'),
+        ),
+    ]
